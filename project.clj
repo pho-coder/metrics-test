c09a83d3beee77672df9e781ab75050876c4b53f
pho-coder <pho.coderzhang@gmail.com>
2015-07-23 19:07:14 +0800

    fix

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 ]
+                 [metrics-clojure "2.5.1"]]
   :main ^:skip-aot metrics-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/metrics_test/core.clj
+++ src/metrics_test/core.clj
@@ -1,7 +1,19 @@
 (ns metrics-test.core
+  (:require [metrics.reporters.jmx :as jmx]
+            [metrics.core :refer [new-registry]]
+            [metrics.counters :refer [counter inc! value]])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (def JR (jmx/reporter {}))
+  (jmx/start JR)
+  (let [reg (new-registry)
+        users-connected (counter reg "users-connected")]
+    (while true
+      (prn (value users-connected))
+      (Thread/sleep 1000)
+      (inc! users-connected 10)))
+  (jmx/stop JR))
